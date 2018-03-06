724b34c3862935a88ac11d978a28ec1870b32596
Antoine Zazzera <antoine.zazzera@influans.com>
2018-03-06 17:02:55 +0100

    remove grunt and use node library to generate files

--- tasks/fontastic.js
+++ tasks/fontastic.js
@@ -1,99 +1,76 @@
-'use strict';
+let request = require('request');
+let fs = require('fs');
+let mkdirp = require('mkdirp');
+let options = require('minimist')(process.argv.slice(2));
+let hostname = 'fontastic.s3.amazonaws.com';
 
-var request = require('request');
+function getBase() {
+    return 'https://' + hostname + '/' + options.key + '/';
+}
 
-module.exports = function(grunt) {
-    grunt.registerMultiTask('fontastic', 'Download fontastic fonts and css', function() {
+function fetchData(uri, isBinary, callback) {
+    let connect = {
+        gzip: true,
+        uri: uri,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'
+        }
+    };
 
-        var done    = this.async(),
-            start   = Date.now(),
-            ready   = 0,
+    if (isBinary) {
+        connect.encoding = 'binary';
+    }
 
-            options = this.options({
-                hostname: 'fontastic.s3.amazonaws.com'
-            });
-
-        function getBase() {
-            return 'https://' + options.hostname + '/' + options.key + '/';
+    request(connect, function(err, res, content) {
+        if (err) {
+            console.log(['Error on fetch', uri, isBinary, err].join(' ;; '));
         }
 
-        function setReady() {
-            ready++;
-            if (ready === 5) {
-                grunt.log.writeln('Task complete in ' + (Date.now() - start) + 'ms');
-                done();
-            }
+        if (res && res.statusCode === 200) {
+            return callback(content);
         }
-
-        function fetchData(uri, isBinary, callback) {
-            var connect = {
-                gzip: true,
-                uri: uri,
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'
-                }
-            };
-
-            if (isBinary) {
-                connect.encoding = 'binary';
-            }
-
-            request(connect, function(err, res, content) {
-                if (err) {
-                    grunt.log.writeln(['Error on fetch', uri, isBinary, err].join(' ;; '));
-                }
-
-                if (res && res.statusCode === 200) {
-                    return callback(content);
-                }
-
-            });
-        }
-
-        function fetchCss(callback) {
-            return fetchData(getBase() + 'icons.css', false, callback);
-        }
-
-        function fetchFont(path, callback) {
-            return fetchData(path, true, callback);
-        }
-
-        startProcess();
-
-        function startProcess() {
-            fetchCss(processCss);
-        }
-
-        function processCss(css) {
-            var uriRegex = /url\("([a-zA-Z0-9:\/\.?#]*?)"\)/g,
-                extRegex = /fonts\/[a-z0-9\.]*?.([a-z]{3,5})/,
-                nameRegex = /font-family: "(.*)"/,
-                remoteNameRegex = /fonts\/([a-z0-9]*)/,
-                result, fontUri, fontName, remoteFontName, localCss;
-
-            fontName = nameRegex.exec(css)[1];
-            remoteFontName = remoteNameRegex.exec(css)[1];
-
-            localCss = css.replace(new RegExp('url\\("https:\/\/file.myfontastic.com\/' + options.key + '\/fonts\/', 'g'), 'url("fonts/');
-            localCss = localCss.replace(new RegExp(remoteFontName, 'g'), fontName);
-
-            grunt.file.write(options.dest + '/' + 'fontastic.css', localCss);
-
-            while ((result = uriRegex.exec(css)) !== null) {
-                fontUri = result[1];
-
-                (function(uri) {
-                    fetchData(result[1], true, function(font) {
-                        processFont(fontName, extRegex.exec(uri)[1], font);
-                    });
-                })(fontUri);
-            }
-        }
-
-        function processFont(fontName, extension, font) {
-            console.log(options.dest + '/fonts/' + fontName + '.' + extension);
-            grunt.file.write(options.dest + '/fonts/' + fontName + '.' + extension, new Buffer(font, 'binary'), { encoding: null });
-        }
-
     });
-};
+}
+
+function processCss(css) {
+    let uriRegex = /url\("([a-zA-Z0-9:\/\.?#]*?)"\)/g,
+        extRegex = /fonts\/[a-z0-9\.]*?.([a-z]{3,5})/,
+        nameRegex = /font-family: "(.*)"/,
+        remoteNameRegex = /fonts\/([a-z0-9]*)/,
+        result, fontUri, fontName, remoteFontName, localCss;
+
+    fontName = nameRegex.exec(css)[1];
+    remoteFontName = remoteNameRegex.exec(css)[1];
+
+    let dirFont = 'fonts/';
+
+    if (options.dirFont) {
+        dirFont = options.dirFont + '/' + dirFont;
+    }
+
+    localCss = css.replace(new RegExp('url\\("https:\/\/file.myfontastic.com\/' + options.key + '\/fonts\/', 'g'), 'url("' + dirFont);
+    localCss = localCss.replace(new RegExp(remoteFontName, 'g'), fontName);
+
+    fs.writeFileSync(options.dest + '/' + 'fontastic.css', localCss);
+
+    while ((result = uriRegex.exec(css)) !== null) {
+        fontUri = result[1];
+
+        (function(uri) {
+            fetchData(result[1], true, function(font) {
+                processFont(fontName, extRegex.exec(uri)[1], font);
+            });
+        })(fontUri);
+    }
+}
+
+function processFont(fontName, extension, font) {
+    let dest = options.dest + '/fonts';
+    console.log(dest + '/' + fontName + '.' + extension);
+
+    mkdirp(dest, () => {
+        fs.writeFileSync(dest + '/' + fontName + '.' + extension, new Buffer(font, 'binary'), { encoding: null });
+    });
+}
+
+fetchData(getBase() + 'icons.css', false, processCss);
